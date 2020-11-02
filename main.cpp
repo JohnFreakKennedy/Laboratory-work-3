27a233354121045963aa9c71ef8f45ede7ccf46f
JohnFreakKennedy <62018542+JohnFreakKennedy@users.noreply.github.com>
2020-11-03 01:41:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    double x,a,s=0,d=1,f=1,b1,b2,an;
-    int k=1,n=2;
-    cin >> x;
+    double x,a,s=0,d=1,f=1;
+    int k=0,n=2;
+    cout<<"x= ";cin >> x;
     do
     {
         d *= x; //збільшення степеня х на 1
@@ -16,6 +16,7 @@ int main()
         s += a; //додавання до змінної суми
         n += 2; //збільшення n з кроком 2
         k++; //ітератор цикла
-    } while (k<=10 && abs(a) >= 0.00001);
-    cout<<fixed<<setprecision(2)<<s << endl;
+     }while (k<=10 || abs(a)> 0.00001);
+    cout<<fixed<<s<< endl;
+    //cout<<fixed<<k<< endl;
 }
