be0f4b44538b343198300546adfe4d6a27e88600
JohnFreakKennedy <62018542+JohnFreakKennedy@users.noreply.github.com>
2020-11-03 01:43:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import math
+while True:
+    try:
+        x = float(input("Введіть число х: "))
+        break
+    except:
+        print("Wrong input")
+k = 0
+n = 2
+s = 0
+d = 1.0
+f = 1.0
+a = 0.0
+while True:
+    d *= x
+    f *= n*(n-1)
+    a = d / f
+    n += 2
+    k += 1
+    s += a
+    if k > 10 and abs(a) < 0.00001:
+        break
+print(s)
+# print(k)
+
